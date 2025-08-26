b239ff37cbc359059a50ec6162488ebfa912ab2a
rayalaramkumar <rayalaramkumar21@gmail.com>
2025-08-26 11:15:38 +0530

    Fix URL patterns and add about view

--- store/views.py
+++ store/views.py
@@ -4,6 +4,9 @@ from .models import Product
 def home(request):
     products = Product.objects.all
     return render(request,'home.html', {'products' :products})
+def about(request):
+    return render(request, 'about.html', {})
+
 
 
 # Create your views here.
